6a2d101e796517f7fb5ec506225c825974ede126
Lee heechan <a_line1@naver.com>
2020-09-24 16:13:56 +0900

    first commit

---
+++ my-private-module/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type    = string
+  default = "ap-northeast-2"
+}
+variable "prefix" {
+  type    = string
+  default = "2020-09-24"
+}
+variable "name" {
+  type    = string
+  default = "s3-for-module"
+}
