e5b86bca2d4be67b67d7b4cfb2944a5b8378338c
Lee heechan <a_line1@naver.com>
2020-09-28 16:44:32 +0900

    test..

--- variables.tf
+++ variables.tf
@@ -1,14 +1,8 @@
 variable "name" {
-  type    = string
-  default = "noname"
 }
 
 variable "region" {
-  type    = string
-  default = "ap-northeast-2"
 }
 
 variable "prefix" {
-  type    = string
-  default = "prefix"
 }
