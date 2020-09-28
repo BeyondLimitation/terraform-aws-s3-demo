98d0a99563e38ed32745412a56c09ffc0441b1d8
Lee heechan <a_line1@naver.com>
2020-09-28 15:46:13 +0900

    folder 'my-private-module' is no longer needed

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  type    = string
+  default = "noname"
+}
+
+variable "region" {
+  type    = string
+  default = "ap-northeast-2"
+}
+
+variable "prefix" {
+  type    = string
+  default = "prefix"
+}
